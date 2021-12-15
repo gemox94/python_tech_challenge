87b5390920d1c9a583f33d348133ccbb5a763c6a
Gerardo <gerardo.mx.rz@gmail.com>
2021-12-15 03:53:48 -0500

    chore: set up get cloudiness description and update params for wind description function

--- weather/helpers.py
+++ weather/helpers.py
@@ -1,11 +1,19 @@
 """Helper functions related to weather app"""
 
 
-def get_wind_description():
+def get_wind_description(wind_speed, wind_degrees, units):
     """Function to get readable wind information"""
     return ''
 
 
 def get_cloudiness_description(cloud_percentage):
     """Function to get readable cloudiness information"""
-    return ''
+    desc = 'Clear sky'
+    if 10 < cloud_percentage <= 50:
+        desc = 'Scattered clouds'
+    elif 50 < cloud_percentage <= 90:
+        desc = 'Broken'
+    elif cloud_percentage > 90:
+        desc = 'Overcast'
+
+    return desc
