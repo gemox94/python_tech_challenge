89603dee9080c989b3853fb230f1c8019c4b1720
Gerardo <gerardo.mx.rz@gmail.com>
2021-12-15 18:33:16 -0500

    test: add tests for city forecasts

--- weather/tests/test_helpers.py
+++ weather/tests/test_helpers.py
@@ -1,11 +1,20 @@
 from django.test import TestCase
 
 from weather.helpers import (
-    get_wind_direction, get_wind_description, get_wind_full_description, get_cloudiness_description
+    get_wind_direction, get_wind_description, get_wind_full_description, get_cloudiness_description, get_city_forecasts
 )
+
+DEGREES_SYMBOL = '°'
 
 
 class WeatherTestHelpers(TestCase):
+
+    def __test_valid_city_forecasts(self, city_forecasts, units):
+        self.assertTrue(len(city_forecasts) > 0)
+        forecast = city_forecasts[0]
+        degrees_unit = f'{DEGREES_SYMBOL}C' if units == 'metric' else f'{DEGREES_SYMBOL}F'
+        self.assertTrue('temperature' in forecast)
+        self.assertTrue(degrees_unit in forecast['temperature'])
 
     def test_get_wind_direction(self):
         self.assertEqual(get_wind_direction('something'), 'Must provide a valid wind degrees')
@@ -53,3 +62,23 @@ class WeatherTestHelpers(TestCase):
         self.assertEqual(get_cloudiness_description(15), 'Scattered clouds')
         self.assertEqual(get_cloudiness_description(51), 'Broken')
         self.assertEqual(get_cloudiness_description(91), 'Overcast')
+
+    def test_get_city_forecasts(self):
+
+        self.assertEqual(
+            get_city_forecasts('unknown', 'wrong_unit', country='mx'),
+            'Invalid units'
+        )
+        self.assertTrue(len(get_city_forecasts('unknown', 'metric', country='mx')) == 0)
+
+        city_forecasts = get_city_forecasts('Puebla', 'metric')
+        self.__test_valid_city_forecasts(city_forecasts, 'metric')
+
+        city_forecasts = get_city_forecasts('Puebla', 'metric', country='mx')
+        self.__test_valid_city_forecasts(city_forecasts, 'metric')
+
+        city_forecasts = get_city_forecasts('Bogota', 'imperial')
+        self.__test_valid_city_forecasts(city_forecasts, 'imperial')
+
+        city_forecasts = get_city_forecasts('Bogota', 'imperial', country='co')
+        self.__test_valid_city_forecasts(city_forecasts, 'imperial')
