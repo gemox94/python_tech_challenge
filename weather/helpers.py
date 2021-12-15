3cb8892fc9976423297eeb6c68b0e394383f3404
Gerardo <gerardo.mx.rz@gmail.com>
2021-12-15 03:18:29 -0500

    chore: create helpers file to get wind and cloudiness readable description

---
+++ weather/helpers.py
@@ -0,0 +1,11 @@
+"""Helper functions related to weather app"""
+
+
+def get_wind_description():
+    """Function to get readable wind information"""
+    return ''
+
+
+def get_cloudiness_description(cloud_percentage):
+    """Function to get readable cloudiness information"""
+    return ''
