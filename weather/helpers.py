23b60d045d24cce523e827c6f3a8f6fc3c85db02
Gerardo <gerardo.mx.rz@gmail.com>
2021-12-15 09:39:24 -0500

    chore: validate wind description input

--- weather/helpers.py
+++ weather/helpers.py
@@ -3,6 +3,15 @@
 
 def get_wind_description(wind_speed, wind_degrees, units):
     """Function to get readable wind information"""
+
+    valid_types = (int, float)
+    if type(wind_speed) not in valid_types or type(wind_degrees) not in valid_types:
+        return 'Must provide valid wind speed and degrees'
+
+    valid_units = ('metric', 'imperial')
+    if units not in valid_units:
+        return 'Invalid units'
+
     return ''
 
 
