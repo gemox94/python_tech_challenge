cc7803984ddb266ea947829903a696ba324d26c9
Gerardo <gerardo.mx.rz@gmail.com>
2021-12-15 09:54:09 -0500

    test: add tests for wind direction

--- weather/tests/test_helpers.py
+++ weather/tests/test_helpers.py
@@ -1,9 +1,20 @@
 from django.test import TestCase
 
-from weather.helpers import get_wind_description, get_cloudiness_description
+from weather.helpers import get_wind_direction, get_wind_description, get_cloudiness_description
 
 
 class WeatherTestHelpers(TestCase):
+
+    def test_get_wind_direction(self):
+        self.assertEqual(get_wind_direction('something'), 'Must provide a valid wind degrees')
+        self.assertEqual(get_wind_direction(337.50), 'north-northwest')
+        self.assertEqual(get_wind_direction(320), 'northwest')
+        self.assertEqual(get_wind_direction(202.50), 'south-southwest')
+        self.assertEqual(get_wind_direction(180), 'south')
+        self.assertEqual(get_wind_direction(145.5), 'southeast')
+        self.assertEqual(get_wind_direction(90), 'east')
+        self.assertEqual(get_wind_direction(48.6), 'northeast')
+        self.assertEqual(get_wind_direction(5.6), 'north')
 
     def test_get_wind_description(self):
         self.assertEqual(
