0407a6e0fb447f51d9064d0a82a9475e79196eb7
Gerardo <gerardo.mx.rz@gmail.com>
2021-12-14 22:47:29 -0500

    test: set up tests for weather endpoint

--- weather/tests/test_views.py
+++ weather/tests/test_views.py
@@ -1,19 +1,57 @@
-from django.test import TestCase
-# from rest_framework.test import APIClient
+from rest_framework.test import APITestCase
 
-# client = APIClient()
+WEATHER_URL = '/api/v1/weather'
+DEGREES_SYMBOL = '°'
 
 
-class WeatherTestViews(TestCase):
+def _is_valid_location(weather, location_name):
+    """Function to validate if a weather response has at least valida keys for a successful response"""
+
+    return (
+            'location_name' in weather and
+            weather['location_name'] == location_name and
+            'temperature' in weather and weather['temperature']
+    )
+
+
+def _is_valid_temperature_unit(weather, unit):
+    unit_to_validate = f'{DEGREES_SYMBOL}C' if unit == 'metric' else f'{DEGREES_SYMBOL}F'
+    return 'temperature' in weather and unit_to_validate in weather['temperature']
+
+
+class WeatherTestViews(APITestCase):
+
+    def __test_temperature_unit(self, unit):
+        res = self.client.get(WEATHER_URL, params={'q': 'bogota,co', 'units': unit})
+        status_code = res.status_code
+        data = res.data
+        self.assertEqual(status_code, 200)
+        self.assertTrue(_is_valid_location(data, 'Bogota, CO'))
+        self.assertTrue(_is_valid_temperature_unit(data, unit))
 
     def test_only_city_success(self):
-        self.assertTrue(True)
+        res = self.client.get(WEATHER_URL, params={'q': 'playa del carmen', 'units': 'metric'})
+        status_code = res.status_code
+        data = res.data
+        self.assertEqual(status_code, 200)
+        self.assertTrue(_is_valid_location(data, 'Playa Del Carmen, MX'))
 
     def test_city_and_state_success(self):
-        self.assertTrue(True)
+        res = self.client.get(WEATHER_URL, params={'q': 'bogota,co', 'units': 'metric'})
+        status_code = res.status_code
+        data = res.data
+        self.assertEqual(status_code, 200)
+        self.assertTrue(_is_valid_location(data, 'Bogota, CO'))
 
-    def test_only_city_fails(self):
-        self.assertTrue(True)
+    def test_invalid_city_fails(self):
+        res = self.client.get(WEATHER_URL, params={'q': 'unknown city', 'units': 'metric'})
+        status_code = res.status_code
+        data = res.data
+        self.assertEqual(status_code, 400)
+        self.assertEqual(data, 'Location not found')
 
-    def test_city_and_state_fail(self):
-        self.assertTrue(True)
+    def test_imperial_units(self):
+        self.__test_temperature_unit('imperial')
+
+    def test_metric_units(self):
+        self.__test_temperature_unit('metric')
